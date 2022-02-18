8efa9749207989f73acb4ed07b20c9f5f2a3b90c
CheffJeff <1999jeffreynijkamp@gmail.com>
2022-02-18 10:33:42 +0100

    Add Database and cocktail + review class

---
+++ CocktailBlog/CocktailBlog/Cocktail.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CocktailBlog
+{
+    class Cocktail
+    {
+        public string CreatorName { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public string Image { get; private set; }
+
+        public List<string> Tools = new List<string>();
+        public List<string> Ingredients = new List<string>();
+
+        public List<Review> Reviews = new List<Review>();
+
+        public Cocktail(string CocktailCreatorName, string CocktailName, string CocktailDesc, 
+            string Img, List<string> CocktailTools, List<string> CocktailIngredients)
+        {
+            CreatorName = CocktailCreatorName;
+            Name = CocktailName;
+            Description = CocktailDesc;
+            Image = Img;
+            Tools = CocktailTools;
+            Ingredients = CocktailIngredients;
+        }
+
+
+    }
+}
---
+++ CocktailBlog/CocktailBlog/Review.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CocktailBlog
+{
+    class Review
+    {
+        public string UserName { get; private set; }
+        public int Rating { get; private set; }
+        public string Description { get; private set; }
+    }
+}
